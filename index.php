7ee2ca37ff9ced7d8c50b0858aecc0b2ed15d177
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-05-29 14:27:24 +0000

    Main Commit

--- index.php
+++ index.php
@@ -1,9 +1,69 @@
-
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <p>Paragraph</p>
-  </body>
+
+<head>
+  <!-- Meta data section -->
+  <meta charset="utf-8">
+  <meta name="description" content="Widget to display numbers in ascending order from given min and max inputs, Using PHP">
+  <meta name="keywords" content="immaculata, icd2o">
+  <meta name="author" content="Atri Sarker">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.png">
+  <link rel="manifest" href="./fav_index/site.webmanifest">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/style.css">
+
+  <!-- MDL -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+  <!-- Title -->
+  <title>Number Display in PHP</title>
+</head>
+
+<body>
+
+  <!-- Header -->
+  <h1>Number Display in PHP</h1>
+
+  <!-- INPUT FORM [used for form validation] -->
+  <form id="inputForm" method="post" action="results.php" target="resultFrame">
+    <!-- Table For Layout -->
+    <table>
+      <tr>
+        <td>
+          <!--  minimum Input -->
+          <label for="minimumInput">minimum:</label>
+          <input id="minimumInput" type="number" name="minimum" value="" step="1" min="" max="" />
+          <br>
+          <!--  maximum Input -->
+          <label for="maximumInput">maximum:</label>
+          <input id="maximumInput" type="number" name="maximum" value="" step="1" min="" max="" />
+          <br>
+        </td>
+        <td>
+          <!-- Image of JS LOGO -->
+          <img class="contentImg" src="./images/phpLogo.svg" alt="PHP Logo!">
+          <br>
+        </td>
+      </tr>
+    </table>
+
+    <!-- Results Button -->
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit"
+      value="submit" id="resultBtn">Get Result!</button>
+  </form>
+
+  <!-- Result -->
+  <iframe name="resultFrame" src="./results.php">
+  </iframe>
+
+</body>
+
 </html>
--- results.php
+++ results.php
@@ -1,16 +1,32 @@
-
-<!-- Result -->
-  <iframe name="resultFrame" src="./results.php">
-  </iframe>
-<!-- <form id="inputForm" method="post" action="results.php" target="resultFrame"> -->
-
+<!-- Result Frame -->
 <b>
   <?php
     // Code
-    if(isset($_POST['submit'])) 
+    if(isset($_POST['minimum'])) 
     {
       // INPUT
-      $input = floatval($_POST['{value}']);
+      $minimum = intval($_POST['minimum']);
+      $maximum = intval($_POST['maximum']);
+
+      // Handle edge case of Maximum being smaller than Minimum
+      if ($maximum < $minimum) {
+        echo "Maximum value must be greater than minimum value!";
+      }
+
+      // Initialize Counter
+      $counter = $minimum;
+
+      // While Loop
+      while ( $counter <= $maximum ) {
+
+        // Display Number
+        echo $counter . " ";
+
+        // Increment counter by 1
+        $counter += 1;
+
+      }
+      
     }
     else {
       // Default
